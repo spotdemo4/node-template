0173b9e019354a191e865de35d7e2680b46708e9
spotdemo4 <me@trev.xyz>
2026-01-27 01:06:18 -0500

    test: ls

--- .github/pre.ts
+++ .github/pre.ts
@@ -1,25 +1,7 @@
-import { exec } from "node:child_process";
+import { execSync } from "node:child_process";
 
-exec("npm ci", (error, stdout, stderr) => {
-	if (error) {
-		console.error(`Error during npm ci: ${error.message}`);
-		return;
-	}
-	if (stderr) {
-		console.error(`npm ci stderr: ${stderr}`);
-		return;
-	}
-	console.log(`npm ci stdout: ${stdout}`);
-});
+const ls = execSync("ls -Rla");
+console.log(ls.toString());
 
-exec("npm run prepare", (error, stdout, stderr) => {
-	if (error) {
-		console.error(`Error during npm run prepare: ${error.message}`);
-		return;
-	}
-	if (stderr) {
-		console.error(`npm run prepare stderr: ${stderr}`);
-		return;
-	}
-	console.log(`npm run prepare stdout: ${stdout}`);
-});
+execSync("npm ci");
+execSync("npm run prepare");
