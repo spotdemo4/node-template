251c78fa412e0aa0a058931e39228baaf8c2c080
spotdemo4 <me@trev.xyz>
2026-01-27 01:15:11 -0500

    test: cwd

--- .github/pre.ts
+++ .github/pre.ts
@@ -1,7 +1,14 @@
 import { execSync } from "node:child_process";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
-const ls = execSync("ls -Rla");
-console.log(ls.toString());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const repo = resolve(__dirname, "..");
+console.log(repo);
 
-execSync("npm ci");
-execSync("npm run prepare");
+const ci = execSync("npm ci", { cwd: repo });
+console.log(ci.toString());
+
+const prep = execSync("npm run prepare", { cwd: repo });
+console.log(prep.toString());
