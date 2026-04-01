4d7e023f8471e3eef101fc80a881759acaf51a80
trev <me@trev.xyz>
2026-04-01 07:54:35 -0400

    chore: use new syntax

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   };
 
   inputs = {
-    systems.url = "github:nix-systems/default";
+    systems.url = "github:spotdemo4/systems";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     trev = {
       url = "github:spotdemo4/nur";
@@ -22,87 +22,63 @@
 
   outputs =
     {
-      nixpkgs,
+      self,
       trev,
       ...
     }:
     trev.libs.mkFlake (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            trev.overlays.packages
-            trev.overlays.libs
-          ];
-        };
-        fs = pkgs.lib.fileset;
-        node = pkgs.nodejs_24;
-        node-slim = pkgs.nodejs-slim_24;
-      in
-      rec {
+      system: pkgs: {
         devShells = {
           default = pkgs.mkShell {
-            name = "dev";
             shellHook = pkgs.shellhook.ref;
             packages = with pkgs; [
               # node
-              node
+              nodejs_24
 
               # lint
               biome
+
+              # format
               nixfmt
               prettier
 
               # util
               bumper
               flake-release
-              renovate
             ];
           };
 
           bump = pkgs.mkShell {
-            name = "bump";
             packages = with pkgs; [
               bumper
             ];
           };
 
           release = pkgs.mkShell {
-            name = "release";
             packages = with pkgs; [
               flake-release
             ];
           };
 
           update = pkgs.mkShell {
-            name = "update";
             packages = with pkgs; [
               renovate
-
-              # npm i
-              node
+              nodejs_24 # npm i
             ];
           };
 
           vulnerable = pkgs.mkShell {
-            name = "vulnerable";
             packages = with pkgs; [
-              # node
-              node
-
-              # nix
-              flake-checker
-
-              # actions
-              octoscan
+              nodejs_24 # node
+              flake-checker # nix
+              octoscan # actions
             ];
           };
         };
 
-        checks = pkgs.lib.mkChecks {
+        checks = pkgs.mkChecks {
           node = {
-            src = packages.default;
+            src = self.packages.${system}.default;
             deps = with pkgs; [
               biome
             ];
@@ -111,24 +87,22 @@
             '';
           };
 
-          nix = {
-            src = fs.toSource {
-              root = ./.;
-              fileset = fs.fileFilter (file: file.hasExt "nix") ./.;
-            };
+          actions = {
+            root = ./.;
+            fileset = ./.github/workflows;
             deps = with pkgs; [
-              nixfmt-tree
+              action-validator
+              octoscan
             ];
-            script = ''
-              treefmt --ci
+            forEach = ''
+              action-validator "$file"
+              octoscan scan "$file"
             '';
           };
 
           renovate = {
-            src = fs.toSource {
-              root = ./.github;
-              fileset = ./.github/renovate.json;
-            };
+            root = ./.github;
+            fileset = ./.github/renovate.json;
             deps = with pkgs; [
               renovate
             ];
@@ -137,51 +111,42 @@
             '';
           };
 
-          actions = {
-            src = fs.toSource {
-              root = ./.;
-              fileset = fs.unions [
-                ./action.yaml
-                ./.github/workflows
-              ];
-            };
+          nix = {
+            root = ./.;
+            filter = file: file.hasExt "nix";
             deps = with pkgs; [
-              action-validator
-              octoscan
+              nixfmt
             ];
-            script = ''
-              action-validator **/*.yaml
-              octoscan scan .
+            forEach = ''
+              nixfmt --check "$file"
             '';
           };
 
           prettier = {
-            src = fs.toSource {
-              root = ./.;
-              fileset = fs.fileFilter (file: file.hasExt "yaml" || file.hasExt "json" || file.hasExt "md") ./.;
-            };
+            root = ./.;
+            filter = file: file.hasExt "yaml" || file.hasExt "json" || file.hasExt "md";
             deps = with pkgs; [
               prettier
             ];
-            script = ''
-              prettier --check .
+            forEach = ''
+              prettier --check "$file"
             '';
           };
         };
 
-        apps = pkgs.lib.mkApps {
-          dev.script = "npm run dev";
+        apps = pkgs.mkApps {
+          dev = "npm run dev";
         };
 
-        packages = with pkgs.lib; rec {
+        packages = with pkgs.lib; {
           default = pkgs.buildNpmPackage (finalAttrs: {
             pname = "node-template";
             version = "0.6.1";
 
-            src = fs.toSource {
+            src = fileset.toSource {
               root = ./.;
-              fileset = fs.difference ./. (
-                fs.unions [
+              fileset = fileset.difference ./. (
+                fileset.unions [
                   ./.github
                   ./.vscode
                   ./flake.nix
@@ -190,7 +155,7 @@
               );
             };
 
-            nodejs = node;
+            nodejs = pkgs.nodejs_24;
             npmConfigHook = pkgs.importNpmLock.npmConfigHook;
             npmDeps = pkgs.importNpmLock {
               npmRoot = finalAttrs.src;
@@ -208,61 +173,26 @@
               mkdir -p $out/{bin,lib/node_modules/node-template}
               cp -r build node_modules package.json $out/lib/node_modules/node-template
 
-              makeWrapper "${getExe node-slim}" "$out/bin/node-template" \
+              makeWrapper "${getExe pkgs.nodejs_24}" "$out/bin/node-template" \
                 --add-flags "$out/lib/node_modules/node-template/build/index.js"
 
               runHook postInstall
             '';
 
             meta = {
-              description = "node template";
               mainProgram = "node-template";
-              homepage = "https://github.com/spotdemo4/node-template";
-              changelog = "https://github.com/spotdemo4/node-template/releases/tag/v${finalAttrs.version}";
+              description = "A template for node.js projects.";
               license = licenses.mit;
               platforms = platforms.all;
+              homepage = "https://github.com/spotdemo4/node-template";
+              changelog = "https://github.com/spotdemo4/node-template/releases/tag/v${finalAttrs.version}";
             };
           });
+        };
 
-          image = pkgs.dockerTools.buildLayeredImage {
-            name = default.pname;
-            tag = default.version;
-
-            contents = with pkgs; [
-              dockerTools.caCertificates
-            ];
-
-            created = "now";
-            meta = default.meta;
-
-            config = {
-              Entrypoint = [ "${meta.getExe default}" ];
-              Labels = {
-                "org.opencontainers.image.title" = default.pname;
-                "org.opencontainers.image.description" = default.meta.description;
-                "org.opencontainers.image.version" = default.version;
-                "org.opencontainers.image.source" = default.meta.homepage;
-                "org.opencontainers.image.licenses" = default.meta.license.spdxId;
-              };
-            };
-          };
-
-          # cross compilation
-          linux-amd64 = deno.compile {
-            package = default;
-            target = "x86_64-unknown-linux-gnu";
-          };
-          linux-arm64 = deno.compile {
-            package = default;
-            target = "aarch64-unknown-linux-gnu";
-          };
-          darwin-arm64 = deno.compile {
-            package = default;
-            target = "aarch64-apple-darwin";
-          };
-          windows-amd64 = deno.compile {
-            package = default;
-            target = "x86_64-pc-windows-msvc";
+        images = {
+          default = pkgs.mkImage self.packages.${system}.default {
+            contents = with pkgs; [ dockerTools.caCertificates ];
           };
         };
 
