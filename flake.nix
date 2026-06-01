dc7447191580bd2df574620cd85ac460d6c91fab
trev <me@trev.xyz>
2026-06-01 02:06:42 -0400

    bump: v0.6.11 -> v0.6.12

--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
           default = pkgs.buildNpmPackage (
             final: with pkgs.lib; {
               pname = "node-template";
-              version = "0.6.11";
+              version = "0.6.12";
 
               src = fileset.toSource {
                 root = ./.;
