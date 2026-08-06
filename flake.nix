5a0afa6b1cdefb63ee3e1b48d1af0b90cd1e42ef
doctor <doctor@trev.xyz>
2026-08-06 22:02:03 +0000

    bump: v0.7.2 -> v0.8.0

--- flake.nix
+++ flake.nix
@@ -88,7 +88,7 @@
           default = pkgs.buildNpmPackage (
             final: with pkgs.lib; {
               pname = "node-template";
-              version = "0.7.2";
+              version = "0.8.0";
 
               src = fileset.toSource {
                 root = ./.;
